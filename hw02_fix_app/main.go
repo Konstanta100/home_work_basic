75d4ccff1f3355eeb5d3214034f3c3aa896d842b
kbatuev <kbatuev@htc-cs.ru>
2024-10-28 22:07:43 +0400

    HW02 is completed

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"github.com/Konstanta100/hw02_fix_app/printer"
+	"github.com/Konstanta100/hw02_fix_app/reader"
+	"github.com/Konstanta100/hw02_fix_app/types"
 )
 
 func main() {
--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -3,7 +3,7 @@ package printer
 import (
 	"fmt"
 
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"github.com/Konstanta100/hw02_fix_app/types"
 )
 
 func PrintStaff(staff []types.Employee) {
