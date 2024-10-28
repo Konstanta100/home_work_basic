e2b763ce8f30a0f8332ae2019bf656d316a21605
kbatuev <kbatuev@htc-cs.ru>
2024-10-28 21:30:56 +0400

    HW02 is completed

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,29 +1,31 @@
-package init
+package main
 
 import (
+	"fmt"
+
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
-func init() {
-	var path string = "data.json"
+func main() {
+	var path string
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
 
-	var err error
-	var staff []types.Employee
-
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
-	staff, err = reader.ReadJSON(path, -1)
+	var staff []types.Employee
+	var err error
 
-	fmt.Print(err)
+	staff, err = reader.ReadJSON(path)
 
-	printer.PrintStaff(staff)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	} else {
+		printer.PrintStaff(staff)
+	}
 }
