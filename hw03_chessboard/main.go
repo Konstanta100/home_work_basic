94f5cb9152ce089a103a3ed0f7f5e8c930b3e4c9
kbatuev <kbatuev@htc-cs.ru>
2024-11-04 12:37:13 +0400

    HW3 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	var width, height int
+
+	fmt.Printf("Введите ширину доски: ")
+	fmt.Scanln(&width)
+	fmt.Printf("Введите высоту доски: ")
+	fmt.Scanln(&height)
+
+	var result, first, last string
+
+	for i := 0; i < height; i++ {
+		if first == "#" {
+			first = " "
+		} else {
+			first = "#"
+		}
+		last = first
+
+		for j := 0; j < width; j++ {
+			if last == "#" {
+				last = " "
+			} else {
+				last = "#"
+			}
+
+			result += last
+		}
+
+		result += "\n"
+	}
+
+	fmt.Printf("%s", result)
 }
